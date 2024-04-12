7dd2bb4d975ed2e68ffb5a25aafd85781afd1dc3
Вадим <den.midav@gmail.com>
2024-04-13 00:17:05 +0300

    init other pages

---
+++ blog/blog.js
@@ -0,0 +1,14 @@
+// document.querySelector('.arrowL').addEventListener('click', function() {
+//     let mainBlock = document.querySelector('.main__block-items');
+//     if(mainBlock) {
+//         mainBlock.className = 'main__block-items-sliderL';
+//         element.style.left = '0px';
+//     }
+// });
+
+// document.querySelector('.arrowR').addEventListener('click', function() {
+//     let mainBlock = document.querySelector('.main__block-items');
+//     if(mainBlock) {
+//         mainBlock.className = 'main__block-items-sliderR';
+//     }
+// });
