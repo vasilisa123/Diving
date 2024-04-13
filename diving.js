70832dbc034432187b7a6dc79266f27d9d2052f1
Вадим <den.midav@gmail.com>
2024-04-13 17:16:57 +0300

    Update diving.js
    Init modal window

--- diving.js
+++ diving.js
@@ -42,5 +42,40 @@ element.style.color = '';
 });
 });
 
-//shop
+//mmodal
+
+let popup = document.querySelector('.header__navigation-item_contacts_button');
+let popupActive = document.querySelector('.popup');
+let header = document.querySelector('header');
+let main = document.querySelector('main');
+let footer = document.querySelector('footer');
+let close = document.querySelector('.popup__exit');
+
+popup.addEventListener('click', function(){
+    popup.classList.add('active');
+    popupActive.classList.toggle('active');
+    header.classList.toggle('active');
+    main.classList.toggle('active');
+    footer.classList.toggle('active');
+});
+
+close.addEventListener('click', function(){
+    popup.classList.remove('active');
+    popupActive.classList.toggle('active');
+    header.classList.toggle('active');
+    main.classList.toggle('active');
+    footer.classList.toggle('active');
+});
+
+document.querySelectorAll('.checkbox__item input[type="checkbox"]').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        if(this.checked) {
+            this.parentNode.style.background = 'linear-gradient(#5FC9F3,#55ABF5)';
+            this.parentNode.style.color = 'white'
+        } else {
+            this.parentNode.style.background = '';
+            this.parentNode.style.color = '' 
+        }
+    });
+});
 
